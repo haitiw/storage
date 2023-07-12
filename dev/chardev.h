25d9731fcc9d80b5001b0a99b7c9aedb95a50e29
haitiw <haitiwplus@gmail.com>
2023-07-12 07:58:53 +0000

    dev

---
+++ dev/chardev.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+*File Name: chardev.h
+*Author: haitiw
+*mail: haitiwplus@gmail.com
+*Created Time: Wed 12 Jul 2023 06:16:23 AM UTC
+ ************************************************************************/
+#ifndef __CHARDEV_H_
+#define __CHARDEV_H_
+
+#define mytype 'f'
+
+typedef struct led_desc{
+	int led_num;    //2 3 4 5
+	int led_state;  //0 or 1
+}led_desc_t;
+
+#define FS_LED_ON  _IOW(mytype,1,led_desc_t)
+#define FS_LED_OFF _IOW(mytype,0,led_desc_t)
+
+
+#endif
+
